7bdfc8b2fbbee5908a9bb8297c398b0b25dbec21
cr21 <cr.tagadiya@gmail.com>
2022-10-29 12:54:55 -0400

    Constants defined for DataIngestion

--- sensor/constant/database.py
+++ sensor/constant/database.py
@@ -1,5 +1,4 @@
-import os
 DATABASE_NAME = 'sensor_data'
 COLLECTION_NAME = 'sensor'
-MONGO_DB_URL=os.getenv('MONGO_DB_URL')
+
 
--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -1,11 +1,29 @@
+import os
 from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
 
-TARGET_COLUMN="class"
+#defining common constant variable for training pipeline
+
+TARGET_COLUMN:str="class"
 PIPELINE_NAME:str="sensor"
 ARTIFACT_DIR:str="artifact"
 FILE_NAME:str="sensor.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
 
+PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
+MODEL_FILE_NAME:str="model.pkl"
+SCHEMA_FILE_PATH:str=os.path.join("config","schema.yaml")
+SCHEMA_DROP_COLUMNS:str = "drop_columns"
 
-PREPROCESSING_OBJECT_FILE_NAME="preprocessing.pkl"
+
+"""
+Data Ingestion related constant starts here:
+Prefix :  DATA_INGESTION 
+"""
+
+DATA_INGESTION_COLLECTION_NAME:str="sensor"
+DATA_INGESTION_DIR_NAME:str="data_ingestion"
+DATA_INGESTION_FEATUER_STORE_DIR:str="feature_store"
+DATA_INGESTION_INGESTED_DIR:str="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
+
