59bc9489380228a2fc4b9075a14fc66f6007c39c
cr21 <cr.tagadiya@gmail.com>
2022-10-29 12:44:21 -0400

    Logger configuration added, Custom Exception added

--- sensor/exception.py
+++ sensor/exception.py
@@ -1,14 +1,11 @@
 import sys
-
+import logging
 
 def error_message_detail(error_message: str, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error occured python script name [{0}] line number[{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, error_message
-    )
-
+    error_message = "Error occured python script name [{0}] line number[{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, error_message)
     return error_message
 
 
@@ -22,6 +19,6 @@ class SensorException(Exception):
         super().__init__(error_message)
 
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
-
+        logging.error(self.error_message)
     def __str__(self):
         return self.error_message
--- sensor/logger.py
+++ sensor/logger.py
@@ -1,8 +1,18 @@
 import logging
 import os
-LOG_FILE_PATH = os.path.join()
+from datetime import datetime
+from from_root import from_root
 
-logging.basicConfig(filename=LOG_FILE_PATH,
-                    format="[]",
+LOG_FILE= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# create logs/<log_file>.log
+LOGS_PATH = os.path.join(from_root(),"logs")
+os.makedirs(LOGS_PATH, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE)
+
+logging.basicConfig(
+                    filename=LOG_FILE_PATH,
+                    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
                     level = logging.INFO
-)
+                )
+
