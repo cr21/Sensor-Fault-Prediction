7d96a39433892b49cde354f545d2a6eb4e8fe86b
cr21 <cr.tagadiya@gmail.com>
2022-10-30 12:12:16 -0400

    DataValidation Configuration Added

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -27,3 +27,16 @@ DATA_INGESTION_FEATUER_STORE_DIR:str="feature_store"
 DATA_INGESTION_INGESTED_DIR:str="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
 
+
+
+"""
+Data Validation related constant starts here:
+Prefix :  DATA_VALIDATION
+"""
+
+DATA_VALIDATION_DIR_NAME:str="data_validation"
+DATA_VALIDATION_VALID_DIR:str="validated"
+DATA_VALIDATION_INVALID_DIR:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+
